77a698a99c1af34ad0a88c6023c1bbb321a8283e
TimerCosmos <yasaswikanchibhotla@gmail.com>
2025-07-02 18:45:28 +0530

    Restricted Mobile Access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,15 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'ChickenHunt';
-
+  ngOnInit(): void {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      alert('Yo hunter, this app is not available on mobile, go on take out your pc, call you friend to play!');
+      window.location.href = 'https://google.com'; // or a landing page, or just show a blank screen
+    }
+  }
 
 
 }
